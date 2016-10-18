ec78e45cf3d0fa1125d09655d3c82340a4870027
Anthony Roussel <anthony.roussel@insa-lyon.fr>
2016-10-18 18:34:15 +0200

    remove useless classes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
 
   public static void main( String[] args ) {
     
-    Config config = new Config();
-    config.setCandidateTimeout( 1500 );
-    
     RaftWorker worker = new RaftWorker();
     worker.createNewNode();
     worker.createNewNode();
